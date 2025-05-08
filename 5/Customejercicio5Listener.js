2dfc719d5db260bd1f1a833e5ad2f2298c1bab09
Veronica22-stack <verosilvaa2206@gmail.com>
2025-05-08 20:39:36 -0300

    Delete 5 directory

--- 5/Customejercicio5Listener.js
+++
@@ -1,9 +0,0 @@
-import ejercicio5Listener from "./generated/CalculatorListener.js";
-
-export class CustomCalculatorListener extends CalculatorListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
